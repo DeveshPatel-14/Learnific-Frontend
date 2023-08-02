ee09fc21a17ce089f1765f1be284cb6caa8fdb8c
DeveshPatel-14 <devpatel1130@gmail.com>
2023-08-02 12:00:24 +0530

    Home component completed ✅

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,336 @@
+import React from "react";
+import "./home.css";
+import {
+  Avatar,
+  Box,
+  Button,
+  HStack,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  Container,
+  Icon,
+  useColorModeValue,
+  createIcon,
+  Flex,
+} from "@chakra-ui/react";
+
+import {
+  FcAbout,
+  FcAssistant,
+  FcCollaboration,
+  FcDonate,
+} from "react-icons/fc";
+
+
+import { Link } from "react-router-dom";
+
+
+const Cards = ({ heading, description, icon, href }) => {
+  return (
+    <Box
+      maxW={{ base: "full", md: "275px" }}
+      w={"full"}
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      p={5}
+    >
+      <Stack align={"start"} spacing={2}>
+        <Flex
+          w={16}
+          h={16}
+          align={"center"}
+          justify={"center"}
+          color={"white"}
+          rounded={"full"}
+          bg={useColorModeValue("gray.100", "gray.700")}
+        >
+          {icon}
+        </Flex>
+        <Box mt={2}>
+          <Heading size="md">{heading}</Heading>
+          <Text mt={1} fontSize={"sm"}>
+            {description}
+          </Text>
+        </Box>
+        <Button variant={"link"} colorScheme={"blue"} size={"sm"}>
+          Learn more
+        </Button>
+      </Stack>
+    </Box>
+  );
+};
+
+const Testimonial = ({ children }) => {
+  return <Box>{children}</Box>;
+};
+
+const TestimonialContent = ({ children }) => {
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      boxShadow={"lg"}
+      p={8}
+      rounded={"xl"}
+      align={"center"}
+      pos={"relative"}
+      _after={{
+        content: `""`,
+        w: 0,
+        h: 0,
+        borderLeft: "solid transparent",
+        borderLeftWidth: 16,
+        borderRight: "solid transparent",
+        borderRightWidth: 16,
+        borderTop: "solid",
+        borderTopWidth: 16,
+        borderTopColor: useColorModeValue("white", "gray.800"),
+        pos: "absolute",
+        bottom: "-16px",
+        left: "50%",
+        transform: "translateX(-50%)",
+      }}
+    >
+      {children}
+    </Stack>
+  );
+};
+
+const TestimonialHeading = ({ children }) => {
+  return (
+    <Heading as={"h3"} fontSize={"xl"}>
+      {children}
+    </Heading>
+  );
+};
+
+const TestimonialText = ({ children }) => {
+  return (
+    <Text
+      textAlign={"center"}
+      color={useColorModeValue("gray.600", "gray.400")}
+      fontSize={"sm"}
+    >
+      {children}
+    </Text>
+  );
+};
+
+const TestimonialAvatar = ({ src, name, title }) => {
+  return (
+    <Flex align={"center"} mt={8} direction={"column"}>
+      <Avatar src={src} alt={name} mb={2} />
+      <Stack spacing={-1} align={"center"}>
+        <Text fontWeight={600}>{name}</Text>
+        <Text fontSize={"sm"} color={useColorModeValue("gray.600", "gray.400")}>
+          {title}
+        </Text>
+      </Stack>
+    </Flex>
+  );
+};
+
+const Home = () => {
+  const Arrow = createIcon({
+    displayName: "Arrow",
+    viewBox: "0 0 72 24",
+    path: (
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0.600904 7.08166C0.764293 6.8879 1.01492 6.79004 1.26654 6.82177C2.83216 7.01918 5.20326 7.24581 7.54543 7.23964C9.92491 7.23338 12.1351 6.98464 13.4704 6.32142C13.84 6.13785 14.2885 6.28805 14.4722 6.65692C14.6559 7.02578 14.5052 7.47362 14.1356 7.6572C12.4625 8.48822 9.94063 8.72541 7.54852 8.7317C5.67514 8.73663 3.79547 8.5985 2.29921 8.44247C2.80955 9.59638 3.50943 10.6396 4.24665 11.7384C4.39435 11.9585 4.54354 12.1809 4.69301 12.4068C5.79543 14.0733 6.88128 15.8995 7.1179 18.2636C7.15893 18.6735 6.85928 19.0393 6.4486 19.0805C6.03792 19.1217 5.67174 18.8227 5.6307 18.4128C5.43271 16.4346 4.52957 14.868 3.4457 13.2296C3.3058 13.0181 3.16221 12.8046 3.01684 12.5885C2.05899 11.1646 1.02372 9.62564 0.457909 7.78069C0.383671 7.53862 0.437515 7.27541 0.600904 7.08166ZM5.52039 10.2248C5.77662 9.90161 6.24663 9.84687 6.57018 10.1025C16.4834 17.9344 29.9158 22.4064 42.0781 21.4773C54.1988 20.5514 65.0339 14.2748 69.9746 0.584299C70.1145 0.196597 70.5427 -0.0046455 70.931 0.134813C71.3193 0.274276 71.5206 0.70162 71.3807 1.08932C66.2105 15.4159 54.8056 22.0014 42.1913 22.965C29.6185 23.9254 15.8207 19.3142 5.64226 11.2727C5.31871 11.0171 5.26415 10.5479 5.52039 10.2248Z"
+        fill="currentColor"
+      />
+    ),
+  });
+
+  return (
+    <section className="home">
+      <div className="container">
+        <Container maxW={"4xl"}>
+          <Stack
+            as={Box}
+            textAlign={"center"}
+            spacing={{ base: 8, md: 14 }}
+            py={{ base: 20, md: 36 }}
+          >
+            <Heading
+              fontWeight={600}
+              fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+              lineHeight={"110%"}
+            >
+              Learn Algorithms And Software Development The 10x Way. <br />
+              <Text as={"span"} color={"green.400"}>
+                Your One-stop coding Junction.
+              </Text>
+            </Heading>
+            <Text color={"gray.500"}>
+              Learnific is an online platform that offers coding courses,
+              coding challenges, and personalized mentorship to help individuals
+              improve their coding skills and prepare for technical interviews.
+            </Text>
+            <Stack
+              direction={"column"}
+              spacing={3}
+              align={"center"}
+              alignSelf={"center"}
+              position={"relative"}
+            >
+              <Link to="/courses">
+                <Button
+                  colorScheme={"green"}
+                  bg={"green.400"}
+                  rounded={"full"}
+                  px={6}
+                  _hover={{
+                    bg: "green.500",
+                  }}
+                >
+                  Get Started
+                </Button>
+              </Link>
+
+              <Box>
+                <Icon
+                  as={Arrow}
+                  color={useColorModeValue("gray.800", "gray.300")}
+                  w={71}
+                  position={"absolute"}
+                  right={-71}
+                  top={"10px"}
+                />
+
+                <Text
+                  fontSize={"lg"}
+                  fontFamily={"Caveat"}
+                  position={"absolute"}
+                  right={"-125px"}
+                  top={"-15px"}
+                  transform={"rotate(10deg)"}
+                >
+                  Starting at ₹99/month
+                </Text>
+              </Box>
+            </Stack>
+          </Stack>
+        </Container>
+      </div>
+
+      <Box padding={"8"} bg={"white.800"}>
+        <Heading
+          children="Services provided by us"
+          textAlign={"center"}
+          fontFamily={"body"}
+          color={"blue.400"}
+        />
+      </Box>
+
+      <div className="container2">
+        <Box p={4}>
+          <Container maxW={"8xl"} mt={"-8vh"}>
+            <Flex flexWrap="wrap" gridGap={6} justify="center">
+              <Cards
+                heading={"Teaching assistant"}
+                icon={<Icon as={FcAssistant} w={10} h={10} />}
+                description={"Teaching assistance provided 24/7"}
+                href={"#"}
+              />
+              <Cards
+                heading={"Community Interaction"}
+                icon={<Icon as={FcCollaboration} w={10} h={10} />}
+                description={
+                  "Interaction between learners community and mentors"
+                }
+                href={"#"}
+              />
+              <Cards
+                heading={"Refund"}
+                icon={<Icon as={FcDonate} w={10} h={10} />}
+                description={
+                  "Full refund given on unstatisfaction regarding the course"
+                }
+                href={"#"}
+              />
+              <Cards
+                heading={"Lifetime Course access"}
+                icon={<Icon as={FcAbout} w={10} h={10} />}
+                description={
+                  "Pay once and access a course for lifetime. Courses updated regurarly"
+                }
+                href={"#"}
+              />
+            </Flex>
+          </Container>
+        </Box>
+      </div>
+
+      <Box bg={useColorModeValue("gray.100", "gray.700")} h={"100vh"}>
+        <Container maxW={"7xl"} py={16} as={Stack} spacing={12}>
+          <Stack spacing={0} align={"center"}>
+            <Heading>Our Clients Speak</Heading>
+            <Text>We have been working with clients around the world</Text>
+          </Stack>
+          <Stack
+            direction={{ base: "column", md: "row" }}
+            spacing={{ base: 10, md: 4, lg: 10 }}
+          >
+            <Testimonial>
+              <TestimonialContent>
+                <TestimonialHeading>Efficient Collaborating</TestimonialHeading>
+                <TestimonialText>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Auctor neque sed imperdiet nibh lectus feugiat nunc sem.
+                </TestimonialText>
+              </TestimonialContent>
+              <TestimonialAvatar
+                src={
+                  "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
+                }
+                name={"Jane Cooper"}
+                title={"CEO at ABC Corporation"}
+              />
+            </Testimonial>
+            <Testimonial>
+              <TestimonialContent>
+                <TestimonialHeading>Intuitive Design</TestimonialHeading>
+                <TestimonialText>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Auctor neque sed imperdiet nibh lectus feugiat nunc sem.
+                </TestimonialText>
+              </TestimonialContent>
+              <TestimonialAvatar
+                src={
+                  "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
+                }
+                name={"Jane Cooper"}
+                title={"CEO at ABC Corporation"}
+              />
+            </Testimonial>
+            <Testimonial>
+              <TestimonialContent>
+                <TestimonialHeading>Mindblowing Service</TestimonialHeading>
+                <TestimonialText>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Auctor neque sed imperdiet nibh lectus feugiat nunc sem.
+                </TestimonialText>
+              </TestimonialContent>
+              <TestimonialAvatar
+                src={
+                  "https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=100&q=80"
+                }
+                name={"Jane Cooper"}
+                title={"CEO at ABC Corporation"}
+              />
+            </Testimonial>
+          </Stack>
+        </Container>
+      </Box>
+
+      
+    </section>
+  );
+};
+
+export default Home;
